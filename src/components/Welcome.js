aa2d5177c4905cc71b6f7b814221b2b5d79692e0
saurabh-08 <66739391+saurabh-08@users.noreply.github.com>
2021-01-03 16:52:09 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ import React, { Component } from "react";
 function Welcome(name) {
   return (
     <div>
-      <h1>`Hey ${name}`</h1>
+      <h1>Hey nam</h1>
       <h2>Welcome to Newton School.</h2>
     </div>
   );
