656ebae474687e8e8d6769c41655f3d2a78453dc
saurabh-08 <66739391+saurabh-08@users.noreply.github.com>
2021-01-03 16:47:46 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,22 @@
 // write code for Welcome component here
+// write code for Welcome component here
+import React, { Component } from "react";
+
+function Welcome(name) {
+  return (
+    <div>
+      <h1>`Hey ${name}`</h1>
+      <h2>Welcome to Newton School.</h2>
+    </div>
+  );
+}
+
+// const Welcome = (name) => <h1>Hey Saurabh</h1>
+
+// class Welcome extends Component {
+//   render() {
+//     return <h1>Hey Saurabh</h1>
+//   }
+// }
+
+export default Welcome;
